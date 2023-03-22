a55ce879456763e7e254c497edab7e4c5df38f61
ziyue.wen <ziyue.wen@chaitin.com>
2023-03-22 21:43:17 +0800

    feat: init

--- pkg/proxy/client.go
+++ pkg/proxy/client.go
@@ -1,1 +1,33 @@
 package proxy
+
+import (
+	"context"
+	"net"
+	"sync"
+)
+
+type ClientManager struct {
+	Port    uint
+	proxyID uint64
+	ctl     net.Conn
+	peers   map[uint64]*clientPeer
+	mu      sync.Mutex
+}
+
+func (c *ClientManager) Dial(ctx context.Context, addr string) error {
+
+}
+
+func (c *ClientManager) waitConnection(ctx context.Context, conn net.Conn) error {
+
+}
+
+type clientPeer struct {
+	connID uint64
+	a      net.Conn
+	b      net.Conn
+}
+
+func (p *clientPeer) startTransfer() {
+
+}

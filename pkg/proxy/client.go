c1a22b93a0efd865c8e97c3aca15fbc94a743033
ziyue.wen <ziyue.wen@chaitin.com>
2023-03-23 18:29:10 +0800

    feat: 客户端

--- pkg/proxy/client.go
+++ pkg/proxy/client.go
@@ -6,6 +6,15 @@ import (
 	"sync"
 )
 
+func NewClientManager(port uint) *ClientManager {
+	m := &ClientManager{
+		Port:  port,
+		peers: make(map[uint64]*clientPeer),
+	}
+
+	return m
+}
+
 type ClientManager struct {
 	Port    uint
 	proxyID uint64
@@ -15,11 +24,40 @@ type ClientManager struct {
 }
 
 func (c *ClientManager) Dial(ctx context.Context, addr string) error {
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		return err
+	}
 
+	msg := createMessage(MessageTypeRegister, &MessageRegister{
+		Port: c.Port,
+	})
+
+	_, err = msg.Write(conn)
+	if err != nil {
+		return err
+	}
+
+	rawMsg, err := readMessage(conn)
+	if err != nil {
+		return err
+	}
+
+	var ack *MessageRegisterAck
+	if v, ok := assertMessage(rawMsg).(*MessageRegisterAck); !ok {
+		return ErrBadConnection
+	} else {
+		ack = v
+	}
+
+	c.proxyID = ack.ID
+	c.ctl = conn
+
+	return nil
 }
 
 func (c *ClientManager) DialTLS(ctx context.Context, addr string) error {
-
+	return nil
 }
 
 func (c *ClientManager) register() {
@@ -27,7 +65,7 @@ func (c *ClientManager) register() {
 }
 
 func (c *ClientManager) waitConnection(ctx context.Context, conn net.Conn) error {
-
+	return nil
 }
 
 type clientPeer struct {
